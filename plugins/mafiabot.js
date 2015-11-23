3cfac1013c87968b688937b22ce4981965f09bc9
MR Wirth <dreikin@gmail.com>
2015-11-23 00:08:40 -0500

    Update with several prototypes for new commands; add config example.
    Includes some formatting and other minor changes.

--- plugins/mafiabot.js
+++ plugins/mafiabot.js
@@ -39,7 +39,7 @@ exports.defaultConfig = {
      * @type {string[]}
      */
     messages: [
-        '@%username% has voted for @%???%',
+        'Command invalid or no command issued. Try the `help` command.'
     ],
     db: './mafiadb'
 };
@@ -63,25 +63,53 @@ exports.mentionHandler = function mentionHandler(_, topic, post) {
     internals.browser.createPost(topic.id, post.post_number, reply, () => 0);
 };
 
-exports.joinHandler = function joinHandler(command) {
+exports.echoHandler = function echoHandler(command) {
     const text = 'topic: ' + command.post.topic_id + '\n'
-             + 'post: ' + command.post.post_number + '\n'
-             + 'input: `' + command.input + '`\n'
-             + 'command: `' + command.command + '`\n'
-             + 'args: `' + command.args + '`\n'
-             + 'mention: `' + command.mention + '`\n'
-             + 'post:\n[quote]\n' + command.post.cleaned + '\n[/quote]';
+               + 'post: ' + command.post.post_number + '\n'
+               + 'input: `' + command.input + '`\n'
+               + 'command: `' + command.command + '`\n'
+               + 'args: `' + command.args + '`\n'
+               + 'mention: `' + command.mention + '`\n'
+               + 'post:\n[quote]\n' + command.post.cleaned + '\n[/quote]';
     internals.browser.createPost(command.post.topic_id, command.post.post_number, text, () => 0);
 };
 
 exports.voteHandler = function voteHandler(command) {
-    const text = '@' + command.post.username + ' voted for ' + command.args[0]
+    let text = '';
+    /* if not in players, tell user to `join`. */
+    if (false) {
+        text = '@' + command.post.username + ': You are not yet a player.\n'
+             + 'Please use `@' + internals.configuration.username + ' join` to join the game.';
+    } else {
+    /* if in players, repeat the vote and add to db. */
+        text = '@' + command.post.username + ' voted for ' + command.args[0]
              + ' in post #<a href="https://what.thedailywtf.com/t/'
              + command.post.topic_id + '/' + command.post.post_number + '">'
              + command.post.post_number + '</a>.\n\n'
              + 'Vote text:\n[quote]\n' + command.input + '\n[/quote]';
+    }
     internals.browser.createPost(command.post.topic_id, command.post.post_number, text, () => 0);
 };
+
+exports.joinHandler = function joinHandler(command) {};
+exports.killHandler = function killHandler(command) {};
+exports.dayHandler = function dayHandler(command) {};
+exports.listVotesHandler = function listVotesHandler(command) {};
+exports.listAllVotesHandler = function listAllVotesHandler(command) {};
+exports.listPlayersHandler = function listPlayersHandler(command) {};
+exports.listAllPlayersHandler = function listAllPlayersHandler(command) {};
+
+function registerCommands(events) {
+    events.onCommand('echo', 'echo a bunch of post info (for diagnostic purposes)', exports.echoHandler, () => 0);
+    events.onCommand('for', 'vote for a player to be executed', exports.voteHandler, () => 0);
+    events.onCommand('join', 'join current mafia game', exports.joinHandler, () => 0);
+    events.onCommand('kill', 'kill a player (mod only)', exports.killHandler, () => 0);
+    events.onCommand('list-all-players', 'list all players, dead and alive', exports.listAllPlayersHandler, () => 0);
+    events.onCommand('list-all-votes', 'list all votes from the game\'s start', exports.listAllVotesHandler, () => 0);
+    events.onCommand('list-players', 'list all players still alive', exports.listPlayersHandler, () => 0);
+    events.onCommand('list-votes', 'list all votes from the day\'s start', exports.listVotesHandler, () => 0);
+    events.onCommand('new-day', 'move on to a new day (mod only)', exports.dayHandler, () => 0);
+}
 
 /**
  * Prepare Plugin prior to login
@@ -105,8 +133,7 @@ exports.prepare = function prepare(plugConfig, config, events, browser) {
     internals.configuration = config.mergeObjects(true, exports.defaultConfig, plugConfig);
     internals.db = engine.Db(internals.configuration.db, {});
     events.onNotification('mentioned', exports.mentionHandler);
-    events.onCommand('join', 'join current mafia game', exports.joinHandler, () => 0);
-    events.onCommand('for', 'vote for a player to be executed', exports.voteHandler, () => 0);
+    registerCommands(events);
 };
 
 /**
